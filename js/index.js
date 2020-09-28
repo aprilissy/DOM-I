f0e9e4a44c8d45904f702c6a405a8d66a9f1b67a
April Darger <april.m.darger@gmail.com>
2020-09-28 16:04:40 -0600

    Added images, nav bar, and h1 with breks using index.js.

--- js/index.js
+++ js/index.js
@@ -14,29 +14,79 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
+
+
 // Example: Update the img src for the logo
+/* Logo Image */
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// April's Code Begins Here:
+// Site Images
+/* CTA Image */
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+/* Middle Image */
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+
+
+// Navigation Elements
+const navElements = document.querySelectorAll('a')
+const navigation = []
+
+for (const [key, value] of Object.entries(siteContent.nav)) {
+  navigation.push({
+    key,
+    value: value
+  })
+}
+
+/* loop through nav assigning keys: 3 ways - Nate's help */
+/* 1st Way */
+for (let i = 0; i < navElements.length; i++) {
+  navElements[i].textContent = navigation[i].value
+}
+
+/* 2nd Way */
+// navElements.forEach((ele, idx) => {
+//   ele.textContent = navigation[idx].value
+// })
+
+/* 3rd Way */
+// const func = (ele, idx) => {
+//   ele.textContent = navigation[idx].value
+// }
+// navElements.forEach(func)
+
+
+
+// cta section content
+// h1 content with line breaks
+const ctaH1 = document.querySelector('.cta-text h1')
+ctaH1.innerHTML = siteContent['cta']['h1'].replace(/ /g, '<br /> ')
